743ffada9aaf682b53f63fe441ada96fc4d8d4e9
MudduKrishna <singamsettymuthukrishna18@gmail.com>
2026-06-29 19:28:41 +0530

    initialcommit

--- backend/src/main/java/com/elearning/controller/AuthController.java
+++ backend/src/main/java/com/elearning/controller/AuthController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
-public class AuthController {
+public class AuthController{
     private final AuthService authService;
 
     @PostMapping("/register")
